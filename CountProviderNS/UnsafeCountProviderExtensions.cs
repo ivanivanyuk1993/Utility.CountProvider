517e4a0433bec76b9b82cfd82c761d707b07384b
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2022-01-27 01:43:59 +0300

    Added `RunAtomicallyAsyncProvider` and `RunAtomicallyAsyncProviderTest` to use it in `RunAtomicallyForMultipleResourceIdsAsyncProvider`

--- CountProviderNS/UnsafeCountProviderExtensions.cs
+++ CountProviderNS/UnsafeCountProviderExtensions.cs
@@ -7,8 +7,20 @@ public static class UnsafeCountProviderExtensions
         Interlocked.Increment(ref countProvider.Count);
     }
 
+    public static Task IncrementSafeAsync(this CountProvider countProvider)
+    {
+        Interlocked.Increment(ref countProvider.Count);
+        return Task.CompletedTask;
+    }
+
     public static void IncrementUnsafe(this CountProvider countProvider)
     {
         countProvider.Count += 1;
+    }
+
+    public static Task IncrementUnsafeAsync(this CountProvider countProvider)
+    {
+        countProvider.Count += 1;
+        return Task.CompletedTask;
     }
 }
